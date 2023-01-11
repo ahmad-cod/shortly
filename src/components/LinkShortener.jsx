2ffa9542abc8ca368e7bc0905cab57cdaa9c2e04
ahmad-cod <ahmadadewale800@gmail.com>
2023-01-11 22:05:30 +0000

    Use the short code api with fetch

--- src/components/LinkShortener.jsx
+++ src/components/LinkShortener.jsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from "react"
 
 export default function LinkShortener() {
+    const getLStoredinks = JSON.parse(localStorage.getItem('links')) || []
+    const [url, setUrl] = useState('')
+    const [links, setLinks] = useState(getLStoredinks)
+    const [error, setError] = useState(false)
+
+    useEffect(() => {
+        localStorage.setItem('links', JSON.stringify(links))
+    }, [links])
+
     function handleSubmit(e) {
         e.preventDefault()
-        console.log('Link Submitted')
+        if(url.replaceAll(' ', '') == '') {
+            setError(true)
+            return
+        }else {
+            fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
+                .then(res => res.json())
+                .then(data => {
+                    data.ok ? setLinks([...links, data.result]) : setError(true)
+                })
+                console.log(links)
+        }
+        setUrl('')
     }
     return (
         <form className="link-shortener" onSubmit={handleSubmit}>
-            <input type="text" placeholder="Shorten a link here..." />
+            <input
+              type="text"
+              placeholder="Shorten a link here..."
+            
+              value={url}
+              onFocus={() => setError(false)}
+              onChange={e => setUrl(e.target.value)}
+            />
             <button type="submit" className="cta">Shorten it!</button>
         </form>
     )
