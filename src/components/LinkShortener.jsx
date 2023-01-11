369559d32804c8b2ef6187d4a5580411e7a550f6
ahmad-cod <ahmadadewale800@gmail.com>
2023-01-11 08:33:33 +0000

    Views Optimized

--- src/components/LinkShortener.jsx
+++ src/components/LinkShortener.jsx
@@ -1,7 +1,11 @@
 
 export default function LinkShortener() {
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log('Link Submitted')
+    }
     return (
-        <form className="link-shortener">
+        <form className="link-shortener" onSubmit={handleSubmit}>
             <input type="text" placeholder="Shorten a link here..." />
             <button type="submit">Shorten it!</button>
         </form>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,38 @@
+import { useRef } from "react"
 import logo from "../assets/images/logo.svg"
 
 export default function Navbar() {
+    const toggleRef = useRef(null)
+    const navRef = useRef(null)
+
+    function toggleNav() {
+        const visibility = navRef.current.getAttribute('data-visible')
+        if(visibility === 'false') {
+            navRef.current.setAttribute('data-visible', true)
+            toggleRef.current.setAttribute('aria-expanded', true)
+        }else {
+            navRef.current.setAttribute('data-visible', false)
+            toggleRef.current.setAttribute('aria-expanded', false)
+        }
+
+        console.log(visibility)
+    }
+
     return (
         <header className="">
             <div className="banner">
                 <div className="logo">
                     <img src={logo} alt="Logo" />
                 </div>
-                <button className="mobile-nav-toggle" aria-label="Menu" aria-controls="nav" aria-expanded='false'>
+                <button
+                    className="mobile-nav-toggle"
+                    aria-label="Menu" aria-controls="nav" aria-expanded='false' 
+                    ref={toggleRef}
+                    onClick={toggleNav}
+                >
                 </button>
             </div>
-            <nav id="nav" data-visible='false'>
+            <nav id="nav" data-visible='false' ref={navRef}>
                     <ul className="left-links">
                         <li>Features</li>
                         <li>Pricing</li>
